cb0c3d4cf601e51fe65da9903d87d4d7874e5c4c
MatheusTomazella <matheustomazella250504@gmail.com>
2020-12-12 22:25:00 -0300

    2020-12-12

---
+++ server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const process   = require( 'process' );
+const express   = require( 'express' );
+const app       = express();
+const http      = require( 'http' ).Server( app );
+const fs        = require( 'fs' );
+
+app.use( express.static(__dirname + '/public') );
+ 
+/* Imagens */
+app.get( '/pics', ( request, response ) => {
+    const files = fs.readdirSync( './public/gallery', { withFileTypes: true }, 'utf-8' );
+    const fileNames = [ ];
+    files.forEach( file => {
+        fileNames.push( file.name );
+    } )
+    //console.log( fileNames );
+    response.json( fileNames );
+} )
+
+/* Index.html */
+app.get( '/', function( request, response ){
+    response.sendFile( __dirname + '/public/index.html' );
+} );
+
+/* Gallery.html */
+app.get( '/gallery', function( request, response ){
+    response.sendFile( __dirname + '/public/gallery.html' );
+} );
+
+/* Support.html */
+app.get( '/support', function( request, response ){
+    response.sendFile( __dirname + '/public/support.html' );
+} );
+
+/* SupportChat.html */
+app.get( '/support-chat', function( request, response ){
+    response.sendFile( __dirname + '/public/supportChat.html' );
+} );
+
+app.get( '/', ( request, response ) => {
+    response.sendFile( `${__dirname}/public/html/index.html` );
+} )
+
+http.listen( process.env.PORT || 3306, ( error ) => {
+    if ( error ) throw error;
+    console.log( 'Server Started' );
+} );
