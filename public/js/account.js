567065eed8853e9398f038e33e295e013f69ee85
Luiz Felipe Costa Soares <lfcsoares04@gmail.com>
2021-11-16 08:02:00 -0300

    Página de usuário

--- public/js/account.js
+++ public/js/account.js
@@ -112,3 +112,69 @@ function editUser ( ) {
         }
     });
 }
+
+function openForm ( ) {
+    document.getElementById("join").style.display = "none";
+    document.getElementById("edit").style.display = "flex";
+    document.getElementById("edit-btn").innerText = "Cadastrar-se";
+    document.getElementById("edit-btn").onclick = createVolunteer;
+    document.getElementById("edit-del").style.display = "none";
+}
+
+function verifyFormValues ( ) {
+    const validStates = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"] ;
+    if ( contEmail.value.trim() == "" ) return false;
+    if ( country.value.trim()   == "" ) return false;
+    if ( state.value.trim() == "" || !validStates.includes(state.value) ) return false;
+    if ( city.value.trim()     == "" ) return false;
+
+    return true;
+}
+
+function createVolunteer ( ) {
+    if ( !verifyFormValues() ) return;
+    $.ajax({
+        type: "POST",
+        url: `${APIurl}/volunt`,
+        data: {
+            id: user.id,
+            isInstitution: 0,
+            nameVisibility: nameVis.checked ? 1 : 0,
+            whatsapp: phone.value,
+            country: country.value,
+            city: city.value,
+            state: state.value,
+            description: description.value,
+            contactEmail: contEmail.value,
+            token: user.token,
+        },
+        success: console.log,
+        error: console.error
+    });
+}
+
+function editVolunteer ( ) {
+    if ( !verifyFormValues() ) return;
+    $.ajax({
+        type: "PUT",
+        url: `${APIurl}/volunt`,
+        data: {
+            identifier: {
+                id: user.id,
+            },
+            update: { 
+                nameVisibility: nameVis.checked ? 1 : 0,
+                whatsapp: phone.value,
+                country: country.value,
+                city: city.value,
+                state: state.value,
+                description: description.value,
+                contactEmail: contEmail.value,
+            },
+            token: user.token,
+        },
+        contentType: "application/json",
+        success: console.log,
+        error: console.error
+    });
+}
